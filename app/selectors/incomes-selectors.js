b0db989e5f171c4aee6134bc4cd8de140734d540
Julio García <julioggonz@gmail.com>
2017-02-17 16:57:17 -0500

    fix old stored clients

--- app/selectors/incomes-selectors.js
+++ app/selectors/incomes-selectors.js
@@ -1,11 +1,12 @@
 import { createSelector } from 'reselect';
+import { List } from 'immutable';
 
 const incomesSelector = (state) => state.getIn(['incomes', 'incomes']);
 const sortedIncomeIdsSelector = (state) => state.getIn(['incomes', 'sortedIncomeIds']);
 
 const getSortedIncomes = createSelector(
   [sortedIncomeIdsSelector, incomesSelector],
-  (sortedIncomeIds, incomesMap) =>
+  (sortedIncomeIds = List(), incomesMap) =>
     sortedIncomeIds.map((incomeId) => incomesMap.get(incomeId)).toList()
 );
 
